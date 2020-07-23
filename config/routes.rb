e20a7a42720e764215268b4d70ea8adbb45bc8ec
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-22 19:18:00 -0500

    adding show action

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
     get "/chairs" => "chairs#index"
+  
+    get "/chairs/:id" => "chairs#show"
   # end
 end
